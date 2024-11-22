5df8dfa0f028450a8927763cf3e9f4928d4ce0f6
eVOIDe <85895413+eVOIDe@users.noreply.github.com>
2024-11-22 17:36:05 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from firebase_admin import credentials, db, initialize_app, firestore
 def auth_db():
     cred = credentials.Certificate("credentials.json")
-    initialize_app(cred, {"databaseURL": "https://tabua-de-mare-ebr-default-rtdb.firebaseio.com/"})
+    initialize_app(cred, {"databaseURL": cred.DATAURL})
     return firestore.client()
